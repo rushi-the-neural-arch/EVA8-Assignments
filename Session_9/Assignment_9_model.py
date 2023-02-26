1753ed3b0449672dd8915214bd89f510bf80a6ee
Rushirajsinh Parmar <34182074+rushi-the-neural-arch@users.noreply.github.com>
2023-02-26 19:56:42 +0530

    Fixed a bug in the model architecture

--- Session_9/Assignment_9_model.py
+++ Session_9/Assignment_9_model.py
@@ -4,6 +4,10 @@ import torch.nn.functional as F
 from torchsummary import summary
 import numpy as np
 
+import torch.nn as nn
+import torch.nn.functional as F
+from torchsummary import summary
+
 class Ultimus_Block(nn.Module):
   def __init__(self):
     super(Ultimus_Block, self).__init__()
@@ -29,13 +33,13 @@ class Transformer(nn.Module):
   def __init__(self):
     super(Transformer, self).__init__()
 
-    self.conv1 = nn.Conv2d(3, 16, 5, bias=False)      # Output - 16x32x32
+    self.conv1 = nn.Conv2d(3, 16, 3, padding=1, bias=False)      # Output - 16x32x32
     self.bn1 = nn.BatchNorm2d(16)
-    self.conv2 = nn.Conv2d(16, 32, 5, bias=False)      # Output - 32x32x32
+    self.conv2 = nn.Conv2d(16, 32, 3, padding=1, bias=False)      # Output - 32x32x32
     self.bn2 = nn.BatchNorm2d(32)
-    self.conv3 = nn.Conv2d(32, 48, 5, bias=False)      # Output - 48x32x32
+    self.conv3 = nn.Conv2d(32, 48, 3, padding=1, bias=False)      # Output - 48x32x32
 
-    self.gap = nn.AvgPool2d(20)            # Output - 48x1x1
+    self.gap = nn.AvgPool2d(32)            # Output - 48x1x1
 
     self.ultimus_prime = nn.ModuleList([Ultimus_Block() for i in range(4)])
 
@@ -53,3 +57,4 @@ class Transformer(nn.Module):
 
     x = self.fc(x)
     return F.log_softmax(x, dim=-1)
+
