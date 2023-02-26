fc3ccb00bdf0faf731b1b96bffcd64ca220bdf89
Rushirajsinh Parmar <34182074+rushi-the-neural-arch@users.noreply.github.com>
2023-02-26 13:07:53 +0000

    Added Assignment 9 solutions via upload

---
+++ Session_9/Assignment_9_model.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchsummary import summary
+import numpy as np
+
+class Ultimus_Block(nn.Module):
+  def __init__(self):
+    super(Ultimus_Block, self).__init__()
+    self.fc_k = nn.Linear(48, 8)
+    self.fc_q = nn.Linear(48, 8)
+    self.fc_v = nn.Linear(48, 8)
+    self.out = nn.Linear(8, 48)
+
+  def forward(self, x):
+    
+    k = self.fc_k(x)
+    q = self.fc_q(x)
+    v = self.fc_v(x)
+    AM = F.softmax(torch.matmul(q, torch.transpose(k, 0, 1)), dim=1) / np.sqrt(8)
+    z = torch.matmul(AM, v)
+
+    out = self.out(z)
+    
+    return x + out
+
+
+class Transformer(nn.Module):
+  def __init__(self):
+    super(Transformer, self).__init__()
+
+    self.conv1 = nn.Conv2d(3, 16, 5, bias=False)      # Output - 16x32x32
+    self.bn1 = nn.BatchNorm2d(16)
+    self.conv2 = nn.Conv2d(16, 32, 5, bias=False)      # Output - 32x32x32
+    self.bn2 = nn.BatchNorm2d(32)
+    self.conv3 = nn.Conv2d(32, 48, 5, bias=False)      # Output - 48x32x32
+
+    self.gap = nn.AvgPool2d(20)            # Output - 48x1x1
+
+    self.ultimus_prime = nn.ModuleList([Ultimus_Block() for i in range(4)])
+
+    self.fc = nn.Linear(48, 10)
+
+  def forward(self, x):
+
+    x = F.relu(self.bn1(self.conv1(x)))
+    x = F.relu(self.bn2(self.conv2(x)))
+    x = self.gap(self.conv3(x))
+    x = x.view(-1, 48)
+
+    for i in range(4):
+      x = self.ultimus_prime[i](x)
+
+    x = self.fc(x)
+    return F.log_softmax(x, dim=-1)
---
+++ Session_9/main.py
@@ -0,0 +1,103 @@
+import torch
+import torchvision
+import numpy as np
+import copy
+import torch.nn as nn
+import albumentations as A
+from torchvision import datasets
+from train import fit_model
+from data import AlbumentationImageDataset
+from Assignment_9_model import Custom_Resnet
+from torch_lr_finder import LRFinder
+from torchsummary import summary
+import seaborn as sns
+
+
+SEED = 1
+
+# CUDA?
+cuda = torch.cuda.is_available()
+print("CUDA Available:", cuda)
+
+# For reproducibility
+torch.manual_seed(SEED)
+
+if cuda:
+    torch.cuda.manual_seed(SEED)
+    BATCH_SIZE=512
+else:
+    BATCH_SIZE=32
+
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True )
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True)
+
+
+train_loader = torch.utils.data.DataLoader(AlbumentationImageDataset(trainset, train=True), batch_size=BATCH_SIZE,
+                                          shuffle=True, num_workers=2)
+test_loader = torch.utils.data.DataLoader(AlbumentationImageDataset(testset, train=False), batch_size=BATCH_SIZE,
+                                          shuffle=False, num_workers=1)
+
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+net = Custom_Resnet().to(device)
+print(summary(net, input_size=(3, 32, 32)))
+
+
+exp_net = copy.deepcopy(net).to(device)
+optimizer = torch.optim.Adam(exp_net.parameters(), lr=0.001)
+criterion = nn.NLLLoss()
+lr_finder = LRFinder(exp_net, optimizer, criterion, device=device)
+lr_finder.range_test(train_loader, end_lr=10, num_iter=200)
+lr_finder.plot()
+min_loss = min(lr_finder.history['loss'])
+ler_rate_1 = lr_finder.history['lr'][np.argmin(lr_finder.history['loss'], axis=0)]
+print("Max LR is {}".format(ler_rate_1))
+
+exp_net = copy.deepcopy(net).to(device)
+optimizer = torch.optim.Adam(exp_net.parameters(), lr=ler_rate_1/10)
+criterion = nn.NLLLoss()
+lr_finder = LRFinder(exp_net, optimizer, criterion, device=device)
+lr_finder.range_test(train_loader, end_lr=ler_rate_1*10, num_iter=200)
+lr_finder.plot()
+min_loss = min(lr_finder.history['loss'])
+ler_rate_2 = lr_finder.history['lr'][np.argmin(lr_finder.history['loss'], axis=0)]
+print("Max LR is {}".format(ler_rate_2))
+
+
+ler_rate = ler_rate_2
+print("Determined Max LR is:", ler_rate)
+
+
+train_net_1 = copy.deepcopy(net).to(device)
+optimizer = torch.optim.Adam(train_net_1.parameters(), lr=(ler_rate/10))
+scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, 
+                                                max_lr=ler_rate,
+                                                steps_per_epoch=len(train_loader), 
+                                                epochs=24,
+                                                pct_start=0.2,
+                                                div_factor=10,
+                                                three_phase=False, 
+                                                final_div_factor=50,
+                                                anneal_strategy='linear'
+                                                ) #final_div_factor=100,
+train_net_1, history = fit_model(
+    train_net_1, device=device,
+    criterion = nn.CrossEntropyLoss(),
+    train_loader=train_loader,
+    test_loader=test_loader,
+    optimizer=optimizer, 
+    scheduler=scheduler, 
+    NUM_EPOCHS=24
+)
+
+
+training_acc, training_loss, testing_acc, testing_loss, lr_trend = history
+
+sns.lineplot(x = list(range(1, 25)), y = training_acc)
+sns.lineplot(x = list(range(1, 25)), y = testing_acc)
+sns.lineplot(x = list(range(1, 25)), y = training_loss)
+sns.lineplot(x = list(range(1, 25)), y = testing_loss)
+sns.lineplot(x = list(range(1, len(lr_trend)+1)), y = lr_trend)
